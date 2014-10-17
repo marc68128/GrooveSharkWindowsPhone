df07edbd7d2e0fdf34a916f1cf2d524bf51d60b9
Marc Unterseh <marc@chloro.eu>
2014-10-17 14:20:03 +0200

    Add unit test project

---
+++ GrooveSharkUnitTest/GrooveSharkClientTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrooveSharkClient.Helpers;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace GrooveSharkUnitTest
+{
+    [TestClass]
+    public class GrooveSharkClientTest
+    {
+        [TestMethod]
+        public void TestHmacMd5()
+        {
+            string hash = HmacMd5.Hash("secret",
+                "{'method': 'addUserFavoriteSong', 'parameters': {'songID': 0}, 'header': {'wsKey': 'key', 'sessionID': 'sessionID'}}");
+            Assert.AreEqual("cd3ccc949251e0ece014d620bbf306e7", hash);
+        }
+    }
+}
